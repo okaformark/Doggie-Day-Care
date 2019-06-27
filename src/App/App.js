84cd3134a9c2fa52295956467a644774f52b0265
Mark Okafor <okaformark@gmail.com>
2019-06-26 19:08:17 -0500

    navbar prints

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import './App.scss';
-import myDogs from './dogs';
-import staffs from './employee';
-import DogPen from '../components/dogPen/dogPen';
-import StaffRoom from '../components/StaffRoom/StaffRoom';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 
 class App extends React.Component {
-  state = {
-    dogs: [],
-    employees: [],
-  }
-
-  componentDidMount() {
-    this.setState({ dogs: myDogs, employees: staffs });
-  }
-
   render() {
-    const { dogs, employees } = this.state;
     return (
       <div className="App">
-        <div className="headings">Doggies- A man's bestfriend</div>
-        <DogPen dogs={dogs}/>
-        <div className="headings">Meet Our Employees</div>
-        <StaffRoom employees={employees}/>
+        <MyNavbar />
       </div>
     );
   }
