fe690928a5f9857f8289f6144cb9825533adfd8c
Mark Okafor <okaformark@gmail.com>
2019-06-26 19:10:35 -0500

    navbar and cards print

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
+import Home from '../components/Home/Home';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <MyNavbar />
+        <Home />
       </div>
     );
   }
